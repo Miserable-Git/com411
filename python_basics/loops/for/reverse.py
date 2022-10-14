b59a5dc89b188524430d067c3ab4efbd50b4d594
Cassius <Cassius@local>
2022-10-14 10:43:08 +0100

    added reverse.py in FOR loop

--- python_basics/loops/for/reverse.py
+++ python_basics/loops/for/reverse.py
@@ -1,8 +1,11 @@
 # reverse a list in python
 # The nTH value can also dictate which direction the loop will count DOWN "-1" / UP "1"
 sentence = "sucoP sucoH"
+refactor = ""
 
 print("Identifying...")
 
 for index in range(len(sentence)-1, -1, -1):
-    print(sentence[index])
+    refactor = refactor + sentence[index]
+
+print(refactor)
