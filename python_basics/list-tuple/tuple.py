d7a5d367bacfbffe0b30192cadfa0e8928f1d70d
Cassius <Cassius@local>
2022-11-04 09:35:12 +0000

    added reverse.py in FOR loop

---
+++ python_basics/list-tuple/tuple.py
@@ -0,0 +1,9 @@
+# Tuples are immutable, which means the data is read only.
+# Decalre a tuple by varible = (DATA, DATA, DATA)
+# Tuples can be unpacked (extrapolated) into other variables.
+
+tupletounpack = ("Cassius", "Cyber Security Management")
+
+name, course = tupletounpack
+
+
